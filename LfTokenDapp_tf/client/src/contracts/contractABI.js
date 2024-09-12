b9f4552523ee21bb61e3c9560a22942da15a054c
luisflorenzan <86024563+luisflorenz@users.noreply.github.com>
2024-09-12 15:44:13 +0000

    3.09 commit

--- LfTokenDapp_tf/client/src/contracts/contractABI.js
+++
@@ -1,7 +0,0 @@
-import MyNFT from "../build/contracts/MyNFT.json";
-
-const contractAddress = process.env.REACT_APP_CONTRACT_ADDRESS;
-
-const contractABI = MyNFT.abi;
-
-export { contractAddress, contractABI };
